f7b9be6674308da9ea1a2a5425344bee49278b4c
T. Virág <virmoc411@gmail.com>
2021-04-21 14:18:51 +0200

    Jq átalakíás elkezdve

--- public_html/megjelenites.js
+++ public_html/megjelenites.js
@@ -1,8 +1,4 @@
-window.addEventListener("load",init);
 
-function $(nev){
-    return document.querySelectorAll(nev);
-}
 //var kepek=["","kepek/kep2.jpg","kepek/kep3.jpg"];
 //var cim=["","Nyuszi és cica","Nyuszi és cica"]
 
@@ -23,11 +19,12 @@ var kep3 ={
     };
     
 var kepTomb=[kep1,kep2,kep3];
-
-function init(){
+var index = 0;
+(function(){
+    
     for (var i = 0; i < kepTomb.length; i++) {
     var elem='<div><h3></h3><img src="" alt=""/><p></p></div>';
-        $("article")[0].innerHTML+=elem;
+        $("article").eq(0).append(elem);
     }
 
     console.log(kepTomb);
@@ -40,10 +37,31 @@ function init(){
     for (var i = 0; i < kepTomb.length; i++) {
 //        console.log(kepTomb[i].eleresiut);
         $("article div img").click(betolt);
+        $("#bal").click(bal);
+        $("#jobb").click(jobb);
         
+    });
+}
+function jobb(){
+    $("article img").eq(i).attr(kepTomb[i]["eleresiut"]);
+    $("article h3").eq(i).text(kepTomb[i].cim);
+    $("article p").eq(i).text(kepTomb[i].leiras);
+    
+    if(index ===kepTomb.length){
+        index=0;
     }
     
- 
+}
+function bal(){
+    $("article img").eq(i).attr(kepTomb[i]["eleresiut"]);
+    $("article h3").eq(i).text(kepTomb[i].cim);
+    $("article p").eq(i).text(kepTomb[i].leiras);
+    
+    if(index ===kepTomb.length){
+        index=0;
+    }  
+}
+
 function betolt(){
     var index =this.id;
     console.log(kepTomb[index].eleresiut);
